9c05c54fef4e91cac551d8055d3e7f2036ef566d
ichaxpro <dewiicha161@gmail.com>
2026-08-28 22:24:52 +0700

    feat: update student profile UI

--- 01-week-1-mobile-development-ecosystem-flutter-refresh/lib/main.dart
+++ 01-week-1-mobile-development-ecosystem-flutter-refresh/lib/main.dart
@@ -14,7 +14,11 @@ class MyApp extends StatelessWidget {
           child: Column(mainAxisSize: MainAxisSize.min, children: [
             Icon(Icons.school, size: 72),
             SizedBox(height: 16),
-            Text('Dewi Chalissa Rania', style: TextStyle(fontSize: 24)),
+            Text('Dewi Chalissa Rania', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            SizedBox(height: 9),
+            Text('244107020023', style: TextStyle(fontSize: 18, color: Colors.grey, fontStyle: FontStyle.italic)),
+            SizedBox(height: 7),
+            Text('244107020023@student.polinema.ac.id', style: TextStyle(fontSize: 14)),
             Text('Pemrograman Mobile — Minggu 1'),
           ]),
         ),
